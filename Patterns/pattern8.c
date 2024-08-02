782f5d966456c162d3328e2e076348dba5db1b81
riedhampattel <riedhampattel.tops@gmail.com>
2024-08-02 11:41:39 +0530

    Update pattern8.c

--- Patterns/pattern8.c
+++ Patterns/pattern8.c
@@ -5,3 +5,36 @@
 0 1 0 1
 1 0 1 0 1
 */
+#include<stdio.h>
+int main()
+{
+	int row,i,j,p,q;
+	printf("\nEnter the row number = ");
+	scanf("%d",&row);
+	for(i=1;i<=row;i++)
+	{
+		if(i%2==0)
+		{
+			p = 0;
+			q = 1;
+		}
+		else
+		{
+			p = 1;
+			q = 0;
+		}
+		for(j=1;j<=i;j++)
+		{
+			if(j%2==0)
+			{
+				printf("%d ",q);
+			}
+			else
+			{
+				printf("%d ",p);
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
