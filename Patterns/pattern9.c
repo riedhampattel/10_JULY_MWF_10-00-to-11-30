326671c9c591112d8294ee658c170abe5db58ac6
riedhampattel <riedhampattel.tops@gmail.com>
2024-08-02 11:42:07 +0530

    Update pattern9.c

--- Patterns/pattern9.c
+++ Patterns/pattern9.c
@@ -5,3 +5,25 @@
  * * * *
 * * * * *
 */
+#include<stdio.h>
+int main()
+{
+	int row,i,j,k;
+	printf("\nEnter the row number = ");
+	scanf("%d",&row);
+	int spc = row-1;
+	for(i=1;i<=row;i++)
+	{
+		for(j=spc;j>=1;j--)
+		{
+			printf(" ");
+		}
+		for(k=1;k<=i;k++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+		spc--;
+	}
+	return 0;
+}
