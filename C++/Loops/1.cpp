665c9f98fb5be9ba71d6496ada71c71d36b169f1
riedhampattel <riedhampattel.tops@gmail.com>
2024-09-09 10:47:27 +0530

    loops updated

--- C++/Loops/1.cpp
+++ C++/Loops/1.cpp
@@ -7,3 +7,18 @@ Expected Output :
 ...
 15 X 10 = 150
 */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,i;
+    cout<<"\nEnter any positive number = ";
+    cin>>num;
+
+    for(i=1;i<=10;i++)
+    {
+        cout<<num<<" x "<<i<<" = "<<num*i<<endl;
+    }
+
+    return 0;
+}
--- C++/Loops/2.cpp
+++ C++/Loops/2.cpp
@@ -1,1 +1,38 @@
 /*Write a C++ program to find the Armstrong number for a given range of number.*/
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int start,end,i,num,temp1,digit=0,j,rem,power,sum=0;
+    cout<<"\nEnter the starting point = ";
+    cin>>start;//1
+    cout<<"\nEnter the ending point = ";
+    cin>>end;//9
+    cout<<"\nArmstrong numbers between "<<start<<" and "<<end<<" is = ";
+    for(i=start;i<=end;i++)
+    {
+        num = i;
+        temp1 = i;
+        digit = 0;
+        sum = 0;
+        while(num!=0)
+        {
+            digit++;
+            num = num/10;
+        }
+        for(j=1;j<=digit;j++)
+        {
+            rem = temp1%10;
+            power = pow(rem,digit);
+            sum = sum + power;
+            temp1 = temp1/10;
+        }
+        if(i==sum)
+        {
+            cout<<i<<" ";
+        }
+    }
+
+    return 0;
+}
