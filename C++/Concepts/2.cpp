f264ec2cdf7de1961feb5a0e292ebccef0bb4630
riedhampattel <riedhampattel.tops@gmail.com>
2024-09-20 10:15:27 +0530

    updated

---
+++ C++/Concepts/2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+//friend function
+class Demo
+{
+	private :
+		string name = "Rahul";
+		int x = 10;	
+	public :
+		friend void display(Demo &obj);	
+};
+void display(Demo &obj)
+{
+	cout<<"\nName = "<<obj.name;
+	cout<<"\nX = "<<obj.x;
+}
+int main()
+{
+	Demo d1;
+	display(d1);
+	return 0;
+}
