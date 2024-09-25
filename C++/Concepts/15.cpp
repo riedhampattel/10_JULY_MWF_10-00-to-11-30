f25104ed8648c5bc50e740ab8e42a9f1692b0270
riedhampattel <riedhampattel.tops@gmail.com>
2024-09-25 11:00:19 +0530

    updated

---
+++ C++/Concepts/15.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+class Student
+{
+	private :
+		int roll,sub1,sub2;
+		string name;
+	public :
+		Student(int roll,string name,int sub1,int sub2)
+		{
+			this->roll = roll;
+			this->name = name;
+			this->sub1 = sub1;
+			this->sub2 = sub2;
+		}
+		void grade()
+		{
+			if(sub1+sub2>150)
+			{
+				cout<<"A";
+			}
+			else
+			{
+				cout<<"B";
+			}
+		}
+};
+class Address
+{
+	private :
+		string city;
+		Student *ref;
+	public :
+		Address(string city,Student *ref)
+		{
+			this->city = city;
+			this->ref = ref;
+		}
+		void display()
+		{
+			cout<<"\nStudent name = "<<ref->name;
+			cout<<"\nGrade = "<<ref->grade();
+			cout<<"\nCity = "<<city;
+		}
+};
+int main()
+{
+	Student s1(101,"Rahul",75,65);
+	Address a1("Ahmedabad",&s2);
+	
+	Student s2(102,"Priya",85,90);
+	Address a2("Surat",&s1);
+	
+	a1.display();
+	return 0;
+}
